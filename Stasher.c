fe2f4c598d77ee222eb13aa678e68225931237f0
Aleksandr Pavozkov <99251771+apavozkov@users.noreply.github.com>
2024-10-30 18:57:44 +0300

    Update Stasher.c

--- Stasher.c
+++ Stasher.c
@@ -3,49 +3,49 @@
 #include <string.h>
 #include <unistd.h>
 
-void hide (const char *filename) {
+//Скрытие
+void hide (const char *filename) { // Получаем имя файла
     char hided_name[256];
-    snprintf(hided_name, sizeof(hided_name), "%s.hidden", filename);
+    snprintf(hided_name, sizeof(hided_name), "%s.hidden", filename); // Собираем новое имя
 
-    // Переименование файла
-    if (rename(filename, hided_name) == -1) {
+    if (rename(filename, hided_name) == -1) { // Переименовываем файл и проверяем
         printf("Не удалось исказить файл");
         exit;
     }
 }
 
-void recovery (const char *filename) {
+// Восстановление
+void recovery (const char *filename) { // Получаем имя файла
     const char *suffix = ".hidden";
     size_t filename_len = strlen(filename);
     size_t suffix_len = strlen(suffix);
 
-    if (filename_len > suffix_len && strcmp(filename + filename_len - suffix_len, suffix) == 0) {
+    if (filename_len > suffix_len && strcmp(filename + filename_len - suffix_len, suffix) == 0) { // Проверяем, если ли у имени файла окончание .hidden
         char recovered_name[256];
-        snprintf(recovered_name, sizeof(recovered_name), "%.*s", (int)(filename_len - suffix_len), filename);
+        snprintf(recovered_name, sizeof(recovered_name), "%.*s", (int)(filename_len - suffix_len), filename); // Пытаемся переименовать обратно, исходя из того, что у имени файла окончание .hidden
 
-        if (rename(filename, recovered_name) == -1) {
+        if (rename(filename, recovered_name) == -1) { // Переименовываем файл и проверяем
             printf("Не удалось восстановить файл");
             exit;
         }
     } else {
-        printf("Окончание имени файла не содержит '.hidden' - восстановление невозможно");
+        printf("Окончание имени файла не содержит '.hidden' - восстановление невозможно"); // ыводим ошибку, если имя файла не содержит .hidden
         exit;
     }
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("Неверные данные на входе", argv[0]);
+    if (argc != 3) { 
+        printf("Неверные данные на входе", argv[0]); // Проверяем данные на входе (ожидаем режим и имя файла)
         exit;
     }
 
-    // Выбор действия на основе первого аргумента
-    if (strcmp(argv[1], "hide") == 0) {
+    if (strcmp(argv[1], "hide") == 0) { // Проверяем режим, вызываем нужную функцию
         hide(argv[2]);
     } else if (strcmp(argv[1], "recover") == 0) {
         recovery(argv[2]);
     } else {
-        printf("Неизвестнй режим");
+        printf("Неизвестнй режим"); // Для некорректных входных данных
         exit;
     }
 
